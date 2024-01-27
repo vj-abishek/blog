9216841740373a1f1f9b5658ea81c0d77f1ebb3a
Abishek PY <43115551+vj-abishek@users.noreply.github.com>
2024-01-27 05:43:41 +0000

    wip: changes

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "vj-abishek",
+    name: "Abishek Yohan",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "product engineer",
     bio: "Turning ideas into interactive realities",
